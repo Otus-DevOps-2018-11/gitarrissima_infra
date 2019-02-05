491ad1629255716ad4fe4b21a825b612254027fd
Anna Semenova <astra-88@mail.ru>
2019-02-06 02:49:53 +0300

    homework #8 terraform-1

---
+++ terraform/lb/lb.tf
@@ -0,0 +1,95 @@
+provider "google" {
+  version = "1.4.0"
+  project = "infra-227420"
+  region  = "europe-west1"
+}
+
+resource "google_compute_instance_template" "test" {
+  name        = "appserver-template"
+  description = "This template is used to create app server instances."
+
+  machine_type = "g1-small"
+
+  disk {
+    source_image = "${var.disk_image}"
+    auto_delete  = true
+    boot         = true
+  }
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  tags = ["reddit-app"]
+}
+
+resource "google_compute_instance_group_manager" "appserver" {
+  name = "appserver-igm"
+
+  base_instance_name = "app"
+  instance_template  = "${google_compute_instance_template.test.self_link}"
+  update_strategy    = "NONE"
+  zone               = "europe-west1-b"
+
+  target_size = "${var.count}"
+
+  named_port {
+    name = "http"
+    port = 9292
+  }
+}
+
+resource "google_compute_backend_service" "website" {
+  name        = "my-backend"
+  description = "Our company website"
+  protocol    = "HTTP"
+  timeout_sec = 10
+
+  backend {
+    group = "${google_compute_instance_group_manager.appserver.instance_group}"
+  }
+
+  health_checks = ["${google_compute_http_health_check.default.self_link}"]
+}
+
+resource "google_compute_http_health_check" "default" {
+  name               = "test"
+  port               = 9292
+  request_path       = "/"
+  check_interval_sec = 1
+  timeout_sec        = 1
+}
+
+resource "google_compute_global_forwarding_rule" "default" {
+  name       = "default-rule"
+  target     = "${google_compute_target_http_proxy.default.self_link}"
+  port_range = "80"
+}
+
+resource "google_compute_target_http_proxy" "default" {
+  name        = "test-proxy"
+  description = "a description"
+  url_map     = "${google_compute_url_map.default.self_link}"
+}
+
+resource "google_compute_url_map" "default" {
+  name            = "url-map"
+  description     = "a description"
+  default_service = "${google_compute_backend_service.website.self_link}"
+
+  host_rule {
+    hosts        = ["*"]
+    path_matcher = "allpaths"
+  }
+
+  path_matcher {
+    name            = "allpaths"
+    default_service = "${google_compute_backend_service.website.self_link}"
+
+    path_rule {
+      paths   = ["/*"]
+      service = "${google_compute_backend_service.website.self_link}"
+    }
+  }
+}
---
+++ terraform/lb/variables.tf
@@ -0,0 +1,35 @@
+variable project {
+  description = "Project ID"
+}
+
+variable region {
+  description = "Region"
+  default     = "europe-west1"
+}
+
+variable zone {
+  description = "Zone"
+  default     = "europe-west1-b"
+}
+
+variable public_key_path_test {
+  description = "Path to the public key used for ssh access"
+}
+
+variable public_key_path_test2 {
+  description = "Path to the public key used for ssh access"
+}
+
+variable public_key_path_test3 {
+  description = "Path to the public key used for ssh access"
+}
+
+variable disk_image {
+  description = "Disk image"
+}
+
+variable count {
+  description = "Count of instances"
+  default     = "1"
+}
+
