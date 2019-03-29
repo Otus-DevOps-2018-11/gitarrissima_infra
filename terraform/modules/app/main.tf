be0d091491d856d97b62eedc01d1da0efc15a67c
Anna Semenova <astra-88@mail.ru>
2019-03-29 18:54:32 +0300

    ansible-2 first commit

---
+++ terraform/modules/app/main.tf
@@ -0,0 +1,64 @@
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  tags         = ["reddit-app"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.app_disk_image}"
+    }
+  }
+
+  network_interface {
+    network = "default"
+access_config = {
+nat_ip = "${google_compute_address.app_ip.address}"
+}
+  }
+
+  metadata {
+    ssh-keys = "test:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "test"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo \"DATABASE_URL=${var.mongodb}:27017\" | sudo tee -a /etc/environment",
+    ]
+  }
+
+  provisioner "file" {
+    source      = "${var.source_files}/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "${var.source_files}/deploy.sh"
+  }
+
+}
+
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name    = "allow-puma-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+
+    ports = ["9292"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
